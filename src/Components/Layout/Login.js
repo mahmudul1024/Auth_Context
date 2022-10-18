c505d1f7ea4e83706f22909133a6a4e7c49fc7b0
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-10-18 11:41:24 +0200

    login and regis page done

--- src/Components/Layout/Login.js
+++ src/Components/Layout/Login.js
@@ -1,9 +1,62 @@
+import { parseAttrSelector } from "daisyui/src/lib/postcss-prefixer/utils";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    console.log(email, password);
+  };
   return (
     <div>
-      <h2>login</h2>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  required
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <Link to="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </Link>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
